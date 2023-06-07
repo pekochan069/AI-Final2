ede330c659fce0f4757d8ac53ddab747889e2c2f
pekochan069 <pekochan069@gmail.com>
2023-06-07 21:56:19 +0900

    remove apple mps support

--- core/device.py
+++ core/device.py
@@ -4,7 +4,5 @@ import torch
 def get_device():
     if torch.cuda.is_available():
         return torch.device("cuda:0")
-    elif torch.backends.mps.is_available():
-        return torch.device("mps")
     else:
         return torch.device("cpu")
