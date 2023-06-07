007f42f31814c32210595ad5506b7b4d168691ee
pekochan069 <pekochan069@gmail.com>
2023-06-07 21:57:11 +0900

    Add feature extractor

--- core/networks/SRGAN.py
+++ core/networks/SRGAN.py
@@ -1,8 +1,21 @@
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision
 
 
-class GeneratorResBlock(nn.Module):
+class FeatureExtractor(nn.Module):
+    def __init__(self, net, feature_layer=31):
+        super(FeatureExtractor, self).__init__()
+        self.features = nn.Sequential(
+            *list(net.features.children())[: (feature_layer + 1)]
+        )
+
+    def forward(self, x):
+        return self.features(x)
+
+
+class GResBlock(nn.Module):
     def __init__(self):
         super().__init__()
 
@@ -39,8 +52,11 @@ class GeneratorResBlock(nn.Module):
 
 
 class Generator(nn.Module):
-    def __init__(self, B: int = 16):
+    def __init__(self, B: int = 5, upscale_factor=4):
         super().__init__()
+
+        assert upscale_factor in [2, 4, 8], "Upscale factor must be 2, 4, or 8"
+        upscale_layers = {2: 1, 4: 2, 8: 3}
 
         layers1 = []
         layers2 = []
@@ -59,7 +75,7 @@ class Generator(nn.Module):
         layers1.append(nn.PReLU())
 
         for _ in range(B):
-            layers2.append(GeneratorResBlock())
+            layers2.append(GResBlock())
 
         layers2.append(
             nn.Conv2d(
@@ -73,31 +89,19 @@ class Generator(nn.Module):
         )
         layers2.append(nn.BatchNorm2d(64))
 
-        layers3.append(
-            nn.Conv2d(
-                in_channels=64,
-                out_channels=256,
-                kernel_size=3,
-                stride=1,
-                padding=1,
-                bias=False,
+        for _ in range(upscale_layers[upscale_factor]):
+            layers3.append(
+                nn.Conv2d(
+                    in_channels=64,
+                    out_channels=256,
+                    kernel_size=3,
+                    stride=1,
+                    padding=1,
+                    bias=False,
+                )
             )
-        )
-        layers3.append(nn.PixelShuffle(2))
-        layers3.append(nn.PReLU())
-
-        layers3.append(
-            nn.Conv2d(
-                in_channels=64,
-                out_channels=256,
-                kernel_size=3,
-                stride=1,
-                padding=1,
-                bias=False,
-            )
-        )
-        layers3.append(nn.PixelShuffle(2))
-        layers3.append(nn.PReLU())
+            layers3.append(nn.PixelShuffle(2))
+            layers3.append(nn.PReLU())
 
         layers3.append(
             nn.Conv2d(
@@ -114,15 +118,16 @@ class Generator(nn.Module):
         self.layers2 = nn.Sequential(*layers2)
         self.layers3 = nn.Sequential(*layers3)
 
+        # for m in self.modules():
+        #     if isinstance(m, nn.Conv2d):
+        #         nn.init.kaiming_normal_(m.weight, mode="fan_in")
+
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode="fan_out")
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.constant_(m.bias, 0)
+                nn.init.normal_(m.weight.data, 0.0, 0.02)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.normal_(m.weight.data, 1.0, 0.02)
+                nn.init.constant_(m.bias.data, 0)
 
     def forward(self, X):
         X = self.layers1(X)
@@ -130,8 +135,8 @@ class Generator(nn.Module):
         return self.layers3(X)
 
 
-class DiscriminatorConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride):
+class DConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride):
         super().__init__()
 
         layers = []
@@ -140,7 +145,7 @@ class DiscriminatorConvBlock(nn.Module):
             nn.Conv2d(
                 in_channels=in_channels,
                 out_channels=out_channels,
-                kernel_size=kernel_size,
+                kernel_size=3,
                 stride=stride,
                 padding=1,
                 bias=False,
@@ -160,6 +165,7 @@ class Discriminator(nn.Module):
         super().__init__()
 
         layers = []
+        classifier = []
 
         layers.append(
             nn.Conv2d(
@@ -173,33 +179,36 @@ class Discriminator(nn.Module):
         )
         layers.append(nn.LeakyReLU(0.2, inplace=True))
 
-        layers.append(DiscriminatorConvBlock(64, 64, 3, 2))
-        layers.append(DiscriminatorConvBlock(64, 128, 3, 1))
-        layers.append(DiscriminatorConvBlock(128, 128, 3, 2))
-        layers.append(DiscriminatorConvBlock(128, 256, 3, 1))
-        layers.append(DiscriminatorConvBlock(256, 256, 3, 2))
-        layers.append(DiscriminatorConvBlock(256, 512, 3, 1))
-        layers.append(DiscriminatorConvBlock(512, 512, 3, 2))
-        layers.append(nn.AdaptiveAvgPool2d(1))
+        layers.append(DConvBlock(64, 64, 2))
+        layers.append(DConvBlock(64, 128, 1))
+        layers.append(DConvBlock(128, 128, 2))
+        layers.append(DConvBlock(128, 256, 1))
+        layers.append(DConvBlock(256, 256, 2))
+        layers.append(DConvBlock(256, 512, 1))
+        layers.append(DConvBlock(512, 512, 2))
 
-        layers.append(nn.Conv2d(512, 1024, 1, bias=False))
-        layers.append(nn.LeakyReLU(0.2, inplace=True))
-        layers.append(nn.Conv2d(1024, 1, 1, bias=False))
-        layers.append(nn.Dropout2d(0.2, inplace=True))
+        classifier.append(nn.AdaptiveAvgPool2d(1))
+        classifier.append(nn.Conv2d(512, 1024, 1, bias=False))
+        classifier.append(nn.LeakyReLU(0.2, inplace=True))
+        classifier.append(nn.Conv2d(1024, 1, 1, bias=False))
+        classifier.append(nn.Dropout2d(0.25))
 
         self.layers = nn.Sequential(*layers)
+        self.classifier = nn.Sequential(*classifier)
+
+        # for m in self.modules():
+        #     if isinstance(m, nn.Conv2d):
+        #         nn.init.kaiming_normal_(m.weight, mode="fan_in")
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode="fan_out")
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.constant_(m.bias, 0)
+                nn.init.normal_(m.weight.data, 0.0, 0.02)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.normal_(m.weight.data, 1.0, 0.02)
+                nn.init.constant_(m.bias.data, 0)
 
     def forward(self, X):
         X = self.layers(X)
+        X = self.classifier(X)
         X = X.flatten(1)
         return F.sigmoid(X)
